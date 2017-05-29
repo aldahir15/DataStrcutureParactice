8a1747fee227706a1cee4bd853edca4832f9209b
Aldahir <aldahir@Aldahirs-Macbook-Pro.local>
2017-05-28 18:33:18 -0700

    All problems are done!

--- lib/data_structures.rb
+++ lib/data_structures.rb
@@ -2,7 +2,7 @@
 
 # Write a method that returns the range of its argument (an array of integers).
 def range(arr)
-  # your code goes here
+  arr.max - arr.min
 end
 
 # Write a method that returns a boolean indicating whether an array is in sorted
@@ -10,7 +10,11 @@ end
 # whether its operands are equal, e.g., 2 == 2 => true, ["cat", "dog"] ==
 # ["dog", "cat"] => false
 def in_order?(arr)
-  # your code goes here
+  if arr == arr.sort
+    true
+  else
+    false
+  end
 end
 
 
@@ -18,12 +22,25 @@ end
 
 # Write a method that returns the number of vowels in its argument
 def num_vowels(str)
-  # your code goes here
+  new_str = str.downcase
+  vowels = ["a", "e", "i", "o", "u"]
+  count = 0
+  new_str.chars.each{|x| count += 1 if vowels.include?(x)}
+  count
 end
 
 # Write a method that returns its argument with all its vowels removed.
 def devowel(str)
-  # your code goes here
+  new_str = str.downcase
+  new_word = ""
+  vowels = ["a", "e", "i", "o", "u"]
+  new_str.chars.each{|x|
+    if vowels.include?(x)
+    else
+      new_word += x
+    end
+  }
+  new_word
 end
 
 
@@ -33,26 +50,34 @@ end
 # non-negative integer in descending order and as strings, e.g.,
 # descending_digits(4291) #=> ["9", "4", "2", "1"]
 def descending_digits(int)
-  # your code goes here
+  int.to_s.split("").sort.reverse
 end
 
 # Write a method that returns a boolean indicating whether a string has
 # repeating letters. Capital letters count as repeats of lowercase ones, e.g.,
 # repeating_letters?("Aa") => true
 def repeating_letters?(str)
-  # your code goes here
+  new_str = str.downcase
+  if new_str.split("") == new_str.split("").uniq
+    return false
+  else
+    return true
+  end
 end
 
 # Write a method that converts an array of ten integers into a phone number in
 # the format "(123) 456-7890".
 def to_phone_number(arr)
-  # your code goes here
+  "(#{arr[0..2].join}) #{arr[3..5].join}-#{arr[6..9].join}"
 end
 
 # Write a method that returns the range of a string of comma-separated integers,
 # e.g., str_range("4,1,8") #=> 7
 def str_range(str)
-  # your code goes here
+  new_arr = str.split(",")
+  int_arr = []
+  new_arr.each{|x| int_arr << x.to_i}
+  int_arr.max - int_arr.min
 end
 
 
@@ -63,5 +88,13 @@ end
 # provided. HINT: use the take(num) and drop(num) methods. You won't need much
 # code, but the solution is tricky!
 def my_rotate(arr, offset=1)
-  # your code goes here
+  if offset < 0
+    offset = arr.length - offset.abs
+  elsif offset > arr.length
+    offset = offset % arr.length
+  end
+  offset_values = arr.take(offset)
+  keep_values = arr.drop(offset)
+  keep_values << offset_values
+  keep_values.flatten
 end
